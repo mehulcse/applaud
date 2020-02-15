12766dd2202a604f34beff1301b7a4d9fe5ebfe6
Jitesh Manglani <jitesh.manglani@tech9.com>
2020-02-15 22:59:06 +0530

    Add auto suggestion input
    - Use auto suggestion input for user selection

---
+++ packages/applaud-ui/src/components/user-selector/user-selector.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+// import { useUsersForSelectorQuery } from "../../generated/graphql";
+import AutoComplete, { OptionType } from "../../components/autocomplete-input";
+import { ValueType } from "react-select/src/types";
+
+interface Props {
+  label: string;
+  userIds: number[];
+  onUsersSelected: (userIds: number[]) => void;
+  placeholder?: string;
+  isMulti?: boolean;
+}
+
+function UserSelectorContainer({
+  userIds,
+  label,
+  onUsersSelected,
+  placeholder,
+  isMulti = false
+}: Props) {
+  const [userSearch, setUserSearch] = useState("");
+
+  // const selectedUsersResult = useUsersForSelectorQuery({
+  //   variables: {
+  //     search: "",
+  //     ids: userIds
+  //   },
+  //   skip: !userIds.length
+  // });
+
+  let selectedUsers: ValueType<OptionType> = [];
+
+  // if (selectedUsersResult?.data?.users?.nodes) {
+  //   selectedUsers = selectedUsersResult.data.users.nodes.map(
+  //     user => ({
+  //       label: user.name,
+  //       value: `${user.id}`
+  //     })
+  //   );
+  // }
+
+  // const usersResult = useUsersForSelectorQuery({
+  //   variables: {
+  //     search: userSearch
+  //   },
+  //   fetchPolicy: "network-only"
+  // });
+
+  let users: OptionType[] = [];
+
+  // if (usersResult?.data?.users?.nodes) {
+  //   users = usersResult.data.users.nodes.map(user => ({
+  //     label: user.name,
+  //     value: `${user.id}`
+  //   }));
+  // }
+
+  const onInputChange = (value: string) => {
+    setUserSearch(value);
+  };
+
+  const onValueChange = (option: any) => {
+    if (option && Array.isArray(option)) {
+      const userIds = option.map(val => parseInt(val.value, 10));
+      onUsersSelected(userIds);
+    } else if (option && !Array.isArray(option)) {
+      onUsersSelected([parseInt(option.value, 10)]);
+    } else {
+      onUsersSelected([]);
+    }
+  };
+
+  return (
+    <AutoComplete
+      suggestions={users}
+      isMulti={isMulti}
+      value={selectedUsers}
+      placeholder={placeholder || "Select User"}
+      id="user-select"
+      label={label}
+      isLoading={false} // TODO: set loading bool
+      onInputChange={onInputChange}
+      onValueChange={onValueChange}
+    />
+  );
+}
+
+export default UserSelectorContainer;
