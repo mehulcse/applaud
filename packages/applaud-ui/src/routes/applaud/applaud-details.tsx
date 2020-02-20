766a9c0bd95ad22148e29d622b0a78e358567dc8
niharika <niharika.kulkarni@tech9.com>
2020-02-20 10:57:23 +0530

    add grid to details form

--- packages/applaud-ui/src/routes/applaud/applaud-details.tsx
+++ packages/applaud-ui/src/routes/applaud/applaud-details.tsx
@@ -4,32 +4,36 @@ import {Grid, TextField, Button, Box } from "@material-ui/core";
 
 function ApplaudDetails() {
     return (
-        <Grid xs={6}>
-            <PaperBox>
-                <form className="applaud-details" noValidate autoComplete="off">
-                    <TextField
-                        label="To"
-                        fullWidth
-                        required
-                    />
-                    <TextField
-                        label="Message"
-                        multiline
-                        fullWidth
-                        required
-                        rows="2"
-                    />
-                    <Box marginY={2}>
+        <PaperBox>
+            <form className="applaud-details" noValidate autoComplete="off">
+                <Grid container spacing={3} xs={6}>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="To"
+                            fullWidth
+                            required
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Message"
+                            multiline
+                            fullWidth
+                            required
+                            rows="2"
+                        />
+                    </Grid>
+                    <Grid item>
                         <Button
                             variant="contained"
                             color="primary"
                         >
                             Send
                         </Button>
-                    </Box>
-                </form>
-            </PaperBox>
-        </Grid>
+                    </Grid>
+                </Grid>
+            </form>
+        </PaperBox>
     );
 }
 
