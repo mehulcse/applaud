84b294cd817e6cfac96851e7a3ab8f8f6728ec28
Jitesh Manglani <jjmanglani01@gmail.com>
2020-02-20 18:45:32 +0530

    Fix issues with users table

--- packages/applaud-ui/src/routes/users/users.tsx
+++ packages/applaud-ui/src/routes/users/users.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
-import { Box, FormControlLabel, Grid, Switch, Button, Typography } from "@material-ui/core";
+import {
+  Box,
+  FormControlLabel,
+  Grid,
+  Switch,
+  Button,
+  Typography
+} from "@material-ui/core";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useUsersQuery } from "../../generated/graphql";
 import UsersTable from "./users-table";
 import { SearchInput } from "../../components/search-input";
 import AppIcon from "../../components/app-icon";
-// import AddUserDialog from "./add-user-dialog";
 import { PAGE_LIMIT } from "../../constants/constants";
 import PaperBox from "../../components/paper-box";
 import PageLayout from "../../components/page-layout";
@@ -16,276 +23,14 @@ export default function Users() {
   const [showAddUser, setShowAddUser] = useState(false);
   const [editUserId, setEditUserId] = useState(0);
 
-  // TODO: Query Users
-  const queryResult = {
-    data: {
-      users: {
-        totalCount: 150,
-        nodes: [
-          {
-            id: 1,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 2,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 3,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 4,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 5,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 6,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 7,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 8,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 9,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 10,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 11,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 12,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 13,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 14,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 15,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 16,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 17,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 18,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 19,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-          {
-            id: 20,
-            name: "jitesh",
-            email: "j@tech9.com",
-            team: {
-              id: 1,
-              name: "RCM"
-            },
-            department: {
-              id: 1,
-              name: "IT"
-            }
-          },
-        ]
-      }
-    }
-  };
+  const queryResult = useUsersQuery({
+    variables: {
+      search,
+      limit: PAGE_LIMIT,
+      offset: 0
+    },
+    fetchPolicy: "network-only"
+  });
 
   function onChange(value: string) {
     setSearch(value);
@@ -295,28 +40,29 @@ export default function Users() {
     setShowAddUser(true);
   }
 
-  function closeDialog() {
+  function closeDialog(id?: number) {
     setShowAddUser(false);
-    // TODO: add refetch of queryResult
+    if (id) {
+      queryResult.refetch();
+    }
   }
 
   function onEdit(userId: number) {
     setEditUserId(userId);
   }
 
-  function updateCloseDialog() {
+  function updateCloseDialog(id?: number) {
     setEditUserId(0);
+    if (id) {
+      queryResult.refetch();
+    }
   }
 
   return (
     <PageLayout pageTitle="Users">
       <PaperBox>
         <Box marginY={2}>
-          <Button
-            color="primary"
-            onClick={handleAddUser}
-            variant="outlined"
-          >
+          <Button color="primary" onClick={handleAddUser} variant="outlined">
             <AppIcon icon={faPlus} standardRightMargin />
             Add User
           </Button>
@@ -333,13 +79,14 @@ export default function Users() {
           </Grid>
         </Box>
         <Box marginY={2}>
-          <UsersTable
-            queryResult={queryResult}
-            onEditClick={onEdit}
-          />
+          <UsersTable queryResult={queryResult} onEditClick={onEdit} />
         </Box>
         <AddUserDialog open={showAddUser} onClose={closeDialog} />
-        <UpdateUserDialog userId={editUserId} open={Boolean(editUserId)} onClose={updateCloseDialog} />
+        <UpdateUserDialog
+          userId={editUserId}
+          open={Boolean(editUserId)}
+          onClose={updateCloseDialog}
+        />
       </PaperBox>
     </PageLayout>
   );
