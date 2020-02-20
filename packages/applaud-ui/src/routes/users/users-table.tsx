9bf7817faceb3230346d1ffd4e21e4e7fb45d37a
Jitesh Manglani <jjmanglani01@gmail.com>
2020-02-20 20:40:04 +0530

    Remove update dialog and add link to users/id route

--- packages/applaud-ui/src/routes/users/users-table.tsx
+++ packages/applaud-ui/src/routes/users/users-table.tsx
@@ -4,26 +4,25 @@ import {
   TableBody,
   TableRow,
   TableCell,
-  TableHead,
-  IconButton,
-  Tooltip,
-  Button
+  TableHead
 } from "@material-ui/core";
-import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { UsersQueryResult } from "../../generated/graphql";
 import NoDataAvailable from "../../components/no-data-available";
-import AppIcon from "../../components/app-icon";
+import AppLink from "../../components/app-link";
 import Loader from "../../components/loader";
 import ErrorCard from "../../components/error-card";
-import { PAGE_LIMIT, LOADER_TYPE, SCROLL_THRESHOLD } from "../../constants/constants";
+import {
+  PAGE_LIMIT,
+  LOADER_TYPE,
+  SCROLL_THRESHOLD
+} from "../../constants/constants";
 
-// TODO: remove any type with queryresult type
 interface Props {
-  queryResult: any;
-  onEditClick: (userId: number) => void;
+  queryResult: UsersQueryResult;
 }
 
 function UsersTable(props: Props) {
-  const { queryResult, onEditClick } = props;
+  const { queryResult } = props;
   const { loading, data, error } = queryResult;
 
   const scrollableRef = useRef<Element>(null);
@@ -37,16 +36,15 @@ function UsersTable(props: Props) {
     if (
       !loading &&
       data?.users?.nodes &&
-      data.users.nodes.length <
-      data.users.totalCount &&
+      data.users.nodes.length < data.users.totalCount &&
       scrollableRef?.current &&
       scrollableRef.current.scrollTop + scrollableRef.current.clientHeight >=
-      scrollableRef.current.scrollHeight * SCROLL_THRESHOLD
+        scrollableRef.current.scrollHeight * SCROLL_THRESHOLD
     ) {
       queryResult.fetchMore({
         variables: {
           offset: data.users.nodes.length,
-          limit: PAGE_LIMIT,
+          limit: PAGE_LIMIT
         },
         updateQuery: (
           prev: any,
@@ -55,22 +53,14 @@ function UsersTable(props: Props) {
           if (!fetchMoreResult) {
             return prev;
           }
-          if (
-            prev?.users?.nodes &&
-            fetchMoreResult &&
-            fetchMoreResult.users
-          ) {
+          if (prev?.users?.nodes && fetchMoreResult && fetchMoreResult.users) {
             return Object.assign({}, prev, {
               ...prev,
               users: {
                 ...prev.users,
-                  nodes: [
-                    ...prev.users.nodes,
-                    ...fetchMoreResult.users.nodes
-                  ]
-                }
+                nodes: [...prev.users.nodes, ...fetchMoreResult.users.nodes]
               }
-            );
+            });
           }
           return prev;
         }
@@ -86,49 +76,23 @@ function UsersTable(props: Props) {
 
   function renderTableBody() {
     if (data?.users?.nodes.length) {
-      return (
-        data.users.nodes.map(
-          (
-            user: any,
-            index: number
-          ) => (
-            <TableRow key={user.id} hover id={user.id}>
-              <TableCell>
-                {user.id}
-              </TableCell>
-              <TableCell>
-                {user.fullName}
-              </TableCell>
-              <TableCell>
-                {user.email}
-              </TableCell>
-              <TableCell>
-                {user.team.name}
-              </TableCell>
-              <TableCell>
-                {user.department.name}
-              </TableCell>
-              <TableCell align="center">
-                <Tooltip title="Edit">
-                  <IconButton
-                    color="inherit"
-                    component={Button}
-                    id={`btn-edit_${user.id}`}
-                    aria-label="Edit"
-                    onClick={onEditClick.bind(null, user.id)}
-                  >
-                    <AppIcon icon={faEdit} size="lg" />
-                  </IconButton>
-                </Tooltip>
-              </TableCell>
-            </TableRow>
-          )
-        )
-      );
+      return data.users.nodes.map((user: any, index: number) => (
+        <TableRow key={user.id} hover id={user.id}>
+          <TableCell>
+            <AppLink to={`/users/${user.id}`}>{user.id}</AppLink>
+          </TableCell>
+          <TableCell>
+            <AppLink to={`/users/${user.id}`}>{user.fullName}</AppLink>
+          </TableCell>
+          <TableCell>{user.email}</TableCell>
+          <TableCell>{user.team.name}</TableCell>
+          <TableCell>{user.department.name}</TableCell>
+        </TableRow>
+      ));
     } else if (error) {
       return (
         <TableRow>
-          <TableCell colSpan={6}>
+          <TableCell colSpan={5}>
             <ErrorCard error={error} />
           </TableCell>
         </TableRow>
@@ -136,7 +100,7 @@ function UsersTable(props: Props) {
     }
     return (
       <TableRow>
-        <TableCell colSpan={6}>
+        <TableCell colSpan={5}>
           <NoDataAvailable />
         </TableCell>
       </TableRow>
@@ -167,7 +131,7 @@ function UsersTable(props: Props) {
           </TableCell>
         </TableRow>
       </TableHead>
-      <TableBody style={{overflow: "auto"}} ref={scrollableRef}>
+      <TableBody style={{ overflow: "auto" }} ref={scrollableRef}>
         {!loading && renderTableBody()}
         {loading && <Loader type={LOADER_TYPE.table} />}
       </TableBody>
--- packages/applaud-ui/src/routes/users/users.tsx
+++ packages/applaud-ui/src/routes/users/users.tsx
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
-import {
-  Box,
-  FormControlLabel,
-  Grid,
-  Switch,
-  Button,
-  Typography
-} from "@material-ui/core";
+import { Box, Grid, Button } from "@material-ui/core";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { useUsersQuery } from "../../generated/graphql";
 import UsersTable from "./users-table";
@@ -16,12 +9,10 @@ import { PAGE_LIMIT } from "../../constants/constants";
 import PaperBox from "../../components/paper-box";
 import PageLayout from "../../components/page-layout";
 import AddUserDialog from "./add-user-dialog";
-import UpdateUserDialog from "./update-user-dialog";
 
 export default function Users() {
   const [search, setSearch] = useState("");
   const [showAddUser, setShowAddUser] = useState(false);
-  const [editUserId, setEditUserId] = useState(0);
 
   const queryResult = useUsersQuery({
     variables: {
@@ -42,17 +33,6 @@ export default function Users() {
 
   function closeDialog(id?: number) {
     setShowAddUser(false);
-    if (id) {
-      queryResult.refetch();
-    }
-  }
-
-  function onEdit(userId: number) {
-    setEditUserId(userId);
-  }
-
-  function updateCloseDialog(id?: number) {
-    setEditUserId(0);
     if (id) {
       queryResult.refetch();
     }
@@ -79,14 +59,9 @@ export default function Users() {
           </Grid>
         </Box>
         <Box marginY={2}>
-          <UsersTable queryResult={queryResult} onEditClick={onEdit} />
+          <UsersTable queryResult={queryResult} />
         </Box>
         <AddUserDialog open={showAddUser} onClose={closeDialog} />
-        <UpdateUserDialog
-          userId={editUserId}
-          open={Boolean(editUserId)}
-          onClose={updateCloseDialog}
-        />
       </PaperBox>
     </PageLayout>
   );
