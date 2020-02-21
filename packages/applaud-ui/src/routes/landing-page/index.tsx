93be0c2a4623ed1d81d2a6ebdf02bfaa0b04c209
niharika <niharika.kulkarni@tech9.com>
2020-02-22 00:16:54 +0530

    color changing appbar

--- packages/applaud-ui/src/routes/landing-page/index.tsx
+++ packages/applaud-ui/src/routes/landing-page/index.tsx
@@ -123,9 +123,6 @@ function LandingPage() {
 
     function ColorChangeScroll(props: Props) {
         const { children } = props;
-        // Note that you normally won't need to set the window ref as useScrollTrigger
-        // will default to window.
-        // This is only being set here because the demo is in an iframe.
         const trigger = useScrollTrigger({
             disableHysteresis: true,
             threshold: 0,
