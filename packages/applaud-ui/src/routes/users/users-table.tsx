a1f8c18147f65747f24b8f01b2e2e27a5940095e
Jitesh Manglani <jjmanglani01@gmail.com>
2020-02-23 14:28:04 +0530

    Remove console log

--- packages/applaud-ui/src/routes/users/users-table.tsx
+++ packages/applaud-ui/src/routes/users/users-table.tsx
@@ -54,7 +54,6 @@ class UsersTable extends React.Component<Props> {
           prev: any,
           { fetchMoreResult }: { fetchMoreResult?: any }
         ) => {
-          console.log("Merge data");
           if (!fetchMoreResult) {
             return prev;
           }
